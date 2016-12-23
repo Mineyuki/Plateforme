9e15e9a03780ebda267424d3fa7e0b7584590573
Mineyuki <Mineyuki@github.com>
2016-12-23 20:42:47 +0100

    Changement en boite : accueil > formation

--- Accueil.php
+++ Accueil.php
@@ -97,162 +97,17 @@
 			
 			<div class='row-fluid'>
 
-				<div class="col-lg-10 col-md-offset-1 ">
+				<div>
 					<h1 class='text-success'>Les Formations</h1>
-					
-					<div class="well">
-						<h3 class='text-success'>Formations en Gestion, Comptabilité, Ressources Humaines, Management</h3>
-						<p>DUT Gestion des Entreprises et des Administrations (GEA)</p>
-						<hr>
-						<p>Spécialité : Gestion Ressources Humaines<br/><br/>
-							Objectifs de la formation :
-						</p>
-						<ul>
-							<li>Réaliser des tâches et assumer des responsabilités spécificiques à l’administration du personnel.</li>
-							<li>Participer à la mise en place de méthodes et d’outils de gestion prévisionnelle des effectifs et des compétences.</li>
-							<li>Contribuer à l’élaboration de diagnostics sociaux et au fonctionnement des institutions représentatives du personnel.</li>
-						</ul>
-						<hr>
-						<p>Spécialité : Gestion Comptable et Financière<br/><br/>
-							Objectifs de la formation :
-						</p>
-						<ul>
-							<li>Maîtriser les méthodes, procédures et techniques comptables, financières et budgétaires.</li>
-							<li>Participer à la mise en place d’un système d’informations économiques, comptables et financières.</li>
-							<li>Participer à l’élaboration et au suivi des outils d’aide à la décision dans le domaine économique et financier.</li>
-						</ul>
-						<hr>
-						<p>Spécialité : Gestion et Management des Organisations<br/><br/>
-							Objectifs de la formation :
-						</p>
-						<ul>
-							<li>Acquérir une solide connaissance dans les différents domaines liés à la gestion d’une organisation privée ou publique</li>
-							<li>La formation permet de développer les compétences du gestionnaire généraliste et d’appréhender de façon globale le système entreprise.</li>
-						</ul>
-						<hr>
-						<p>Licence Professionnelle Gestion des Ressources Humaines (GRH)<br/>
-							Spécialité : Métiers de la GRH : Formation, compétences et emploi<br/><br/>
-							Objectifs de la formation :<br/>
-							Acquérir une maîtrise globale des différents domaines des Ressources Humaines : recruter le personnel, administrer le personnel et la paie, gérer les carrières et les relations collectives au sein de l’entreprise, informer et communiquer, etc…
-						</p>
-						<hr>
-						<p>Licence Professionnelle Métiers de la Gestion et de la Comptabilité<br/>
-							Spécialité : Comptabilité et paie<br/><br/>
-							Objectifs de la formation :
-						</p>
-						<ul>
-							<li>Acquérir les compétences nécessaires pour gérer la paie du salarié de son entrée dans l’entreprise à sa sortie</li>
-							<li>Gérer les charges de personnel de l’entreprise</li>
-							<li>S’assurer du respect permanent du droit du travail et de la législation sociale.</li>
-							<li>Être l’interlocuteur privilégié des organismes sociaux et fiscaux.</li>
-							<li>Maîtriser les principaux logiciels de paie et de gestion des ressources humaines.</li>
-						</ul>
-						<p><a href="Formation/GEA.php" class="btn btn-primary">En savoir plus...</a></p>
+					<hr>
+					<div id="conteneur">
+						<a href="Formation/DIU.php" class="element">Modulaire et Diplômante Interuniversitaire</a>
+						<a href="Formation/GEA.php" class="element">Gestion, Comptabilité, Ressources Humaines et Management</a>
+						<a href="Formation/CJ.php" class="element">Juridique, Notariat et Finance</a>
+						<a href="Formation/INFO.php" class="element">Informatique, Systèmes et Logiciels</a>
+						<a href="Formation/RT.php" class="element">Réseaux et Télécommunications</a>
+						<a href="Formation/GEII.php" class="element">Electronique, Electricité, Informatique Industrielle et Nanotechnologies</a>
 					</div>
-      
-					<div class="well">
-						<h3 class='text-success'>Formations en Juridique, Notariat, Finance</h3>
-						<p>DUT Carrières Juridiques<br/><br/>
-							Objectifs de la formation :<br/>
-							Former des assistants juridiques et des attachés commerciaux possédant un solide bagage juridique et une bonne connaissance de l’entreprise et de son environnement.
-						</p>
-							<hr>
-						<p>Licence Professionnelle Banque<br/>
-							Spécialité : Chargé de Clientèle Particuliers<br/><br/>
-							Objectifs de la formation :<br/>
-							Acquérir les compétences nécessaires à un chargé de clientèle particuliers :
-						</p>
-							<ul>
-								<li>Savoir conseiller les clients</li>
-								<li>Gérer un portefeuille clients</li>
-								<li>Maîtriser les outils bancaires</li>
-								<li>Construire un plan de financement</li>
-								<li>Gérer un patrimoine particulier</li>
-								<li>Informer et communiquer sur les produits d’épargne et de placement</li>
-							</ul>
-							<hr>
-						<p>Licence Professionnelle Métiers du Notariat<br/>
-							Objectifs de la formation :<br/>
-							Cette formation a été élaborée en collaboration avec la chambre des Notaires de Paris, pour répondre aux besoins de la profession.
-						</p>
-							<ul>
-								<li>Savoir rédiger un acte (vente, emprunt, notoriété, attestation immobilière, déclaration de succession, statuts de société, donations, etc…)</li>
-								<li>Connaître la nature des pièces administratives préalables et postérieures, indispensables à une opération</li>
-								<li>Avoir des connaissances juridiques approfondies dans les domaines spécifiques au notariat</li>
-								<li>Savoir gérer l’accueil clientèle</li>
-								<li>Savoir apporter des réponses aux questions administratives et juridiques</li>
-							</ul>
-						<p><a href="Formation/CJ.php" class="btn btn-primary">En savoir plus...</a></p>
-					</div>
-      
-					<div class="well">
-						<h3 class='text-success'>Formations en Informatique, Systèmes, Logiciels</h3>
-						<p>DUT Informatique<br/><br/>
-							Objectifs de la formation :<br/>
-							Former des profesionnels ayant une compétence technique large. Compétents au plan technologique, mais capables également d’entretenir des rapports de travail avec les utilisateurs de l’informatique et les titulaires des différentes fonctions dans l’entreprise.
-						</p>
-							<hr>
-						<p>Licence Professionnelle Métiers de l’informatique : Conception, développement, test de logiciels<br/>
-							Spécialité : Génie Logiciel, Système d’information<br/><br/>
-							Objectifs de la formation :
-						</p>
-							<ul>
-								<li>Développer des applications informatiques</li>
-								<li>Mettre en place et personnaliser des applications distribuées</li>
-								<li>Analyser et concevoir une application en utilisant des outils de génie logiciel</li>
-								<li>Gérer des projets technologiques faisant intervenir plusieurs partenaires</li>
-								<li>Assister et conseiller des utilisateurs</li>
-								<li>Intégrer une équipe de développement de projets sur les technologies nouvelles</li>
-							</ul>
-						<p><a href="Formation/INFO.php" class="btn btn-primary">En savoir plus...</a></p>
-					</div>
-
-					<div class="well">
-						<h3 class='text-success'>Formations en Réseaux, Télécommunications</h3>
-						<p>DUT Réseaux et Télécommunications (R&T)<br/><br/>
-							Objectifs de la formation :<br/>
-							Permettre à l’étudiant de maîtriser les Nouvelles Technologies de l’Information et de la Communication (NTIC), qui sont omniprésentes dans nos sociétés. Ces services qui relevaient jusqu’à présent du contexte professionnel (travail collaboratif, Cloud Computing, etc.) arrivent en force dans la vie quotidienne : réseaux sociaux, jeux en ligne, e-commerce, vidéo à la demande, accès mobiles aux services Internet, etc.
-						</p>
-							<hr>
-						<p>
-							Licence Professionnelle Réseaux et Télécommunications<br/>
-							Spécialité : Administration et Sécurité des Réseaux (ASUR)<br/><br/>
-							Objectifs de la formation :<br/>
-							Acquérir de solides connaissances en matières de réseaux, de services et supports de communication afin de bâtir des solutions réseaux en tenant compte de critères fonctionnels, techniques et économiques. L’accent sera mis sur les aspects de sécurité et de qualité. Concevoir, installer, administrer et faire évoluer les solutions réseaux retenues.
-						</p>
-						<p><a href="Formation/RT.php" class="btn btn-primary">En savoir plus...</a></p>
-					</div>
-					
-					<div class="well">
-						<h3 class='text-success'>Formations en Électronique, Électricité, Informatique Industrielle, Nanotechnologies</h3>
-						<p>DUT Génie Électrique et Informatique Industrielle (GEII)<br/><br/>
-							Objectifs de la formation :<br/>
-							Former des techniciens supérieurs opérationnels (Niveau III), capables de s’adapter aux évolutions rapides des technologies diverses et variées du Génie Électrique et de l’Informatique Industrielle (GEII).
-						</p>
-							<hr>
-						<p>Licence Professionnelle Électronique et Informatique Industrielle<br/>
-							Appliquées aux Industries du Transport (E3IT)<br/><br/>
-							Objectifs de la formation :<br/>
-							Conçue dans un objectif d’insertion directe,cette licence vise à former de futurs professionnels en Electronique,Electronique de puissance, Informatique Industrielle et Réseaux spécialisés dans les technologies utilisées par les industriels du transport (automobile,avionique, ferroviaire et maritime). Elle est donc orientée vers un domaine en forte activité dans la région nord de l’Ile de France.
-						</p>
-							<hr>
-						<p>Licence Professionnelle Électronique, Optique et Nanotechnologies<br/><br/>
-							Objectifs de la formation :
-						</p>
-							<ul>
-								<li>Acquérir et/ou consolider vos connaissances en :
-									<ul>
-										<li>Electronique</li>s
-										<li>Optique</li>
-										<li>Techniques de miniaturisation des systèmes (micro - et nanotechnologies)</li>
-									</ul>
-								</li>
-								<li>Développer votre aptitude à la transdisciplinarité.</li>
-								<li>Acquérir un savoir-faire dans notre salle blanche</li>
-							</ul>
-						<p><a href="Formation/GEII.php" class="btn btn-primary">En savoir plus...</a></p>
-					</div>
-					
 				</div>
   
 			</div>
