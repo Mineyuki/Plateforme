e3b37f598458b198868877389adf4429313a69b1
Thelegendarry <co.steph.ballet@gmail.com>
2017-01-08 02:42:16 +0100

    version 3 de la partie Rendez-vous

---
+++ Fonctions.php
@@ -0,0 +1,180 @@
+<?php
+
+require('cnx.php');
+
+class Date{
+	
+	var $days = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
+	var $months = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novemebre', 'Décembre');
+	
+	function getEventsDest($year, $mail){
+		global $bd;
+		$req = $bd->prepare('select id, title, dateh, mailDest from events where year(dateh)= :year and mailDest = :mail');
+		$req->bindValue(':year', $year);
+		$req->bindValue(':mail', $mail);
+		$req->execute();
+		$r = array();
+		while($d = $req->fetch(PDO::FETCH_OBJ)){
+			$r[strtotime($d->dateh)][$d->id] = $d->title;
+		}
+		return $r;
+	}
+	
+	function getEventsExpe($year, $mail){
+		global $bd;
+		$req = $bd->prepare('select id, title, dateh, mailExpe from events where year(dateh)= :year and mailExpe = :mail');
+		$req->bindValue(':year', $year);
+		$req->bindValue(':mail', $mail);
+		$req->execute();
+		$r = array();
+		while($d = $req->fetch(PDO::FETCH_OBJ)){
+			$r[strtotime($d->dateh)][$d->id] = $d->title;
+		}
+		return $r;
+	}
+	
+	function getAll($year){
+		$r = array();
+		/*$date = strtotime($year.'-01-01');
+		
+		while(date('Y', $date) <= $year){
+			$y = date('Y', $date);
+			$m = date('n', $date);
+			$d = date('j', $date);
+			$w = str_replace('0', '7',date('w', $date));
+			$r[$y][$m][$d] = $w;
+			$date =  strtotime(date('Y-m-d', $date).' +1 Day');
+			*/
+		
+		$date = new DateTime($year.'-01-01');
+		
+		while($date->format('Y') <= $year){
+			$y = $date->format('Y');
+			$m = $date->format('n');
+			$d = $date->format('j');
+			$w = str_replace('0', '7',$date->format('w'));
+			$r[$y][$m][$d] = $w;
+			$date->add(new DateInterval('P1D'));
+		}
+		return $r;
+	}
+	
+}
+
+
+/*fonction pour les événements d'étudiants */
+function ajouterEventPerso($date, $title, $mail){
+	global $bd;
+	try{
+		$reqID = $bd->prepare('select LAST_INSERT_ID() as id from events');
+		$reqID->execute();
+		$res = $reqID->fetch(PDO::FETCH_NUM);
+		echo $res[0];
+		$id = $res[0]+1;
+		$req = $bd->prepare('insert into events (dateh, title, mailDest) values ( :dateh, :title, :mail)');
+		$req->bindValue(':dateh', $date);
+		$req->bindValue(':title', $title);
+		$req->bindValue(':mail', $mail);
+		$req->execute();
+		
+	}
+	catch(PDOException $e){
+		die('<p> La connexion a échoué. Erreur['.$e->getCode().'] : '.$e->getMessage().'</p>');
+	}
+}
+
+
+function ajouterEventProfesseur($date, $title, $mailProf, $mailEtu){
+	global $bd;
+	try{
+		$reqID = $bd->prepare('select LAST_INSERT_ID() as id from events');
+		$reqID->execute();
+		$res = $reqID->fetch(PDO::FETCH_NUM);
+		$req = $bd->prepare('insert into events (dateh, title, mailDest, mailExpe) values ( :dateh, :title, :mailDest, :mailExpe)');
+		$req->bindValue(':dateh', $date);
+		$req->bindValue(':title', $title);
+		$req->bindValue(':mailDest', $mailEtu);
+		$req->bindValue(':mailExpe', $mailProf);
+		$req->execute();
+		
+	}
+	catch(PDOException $e){
+		die('<p> La connexion a échoué. Erreur['.$e->getCode().'] : '.$e->getMessage().'</p>');
+	}
+}
+
+
+function genereListeEtudiant(){
+	global $bd;
+	try{
+		$req = $bd->prepare('select distinct mail from membres where categorie ="etudiant"');
+		$req->execute();
+		echo '<select name="selection">';
+		$i=0;
+		do{
+			$res = $req->fetch(PDO::FETCH_NUM);
+			
+			if($res[0] != '')
+				echo '<option value='. $res[0] .'> '. $res[0] . '</option>';
+			
+		}while($res != false);
+		echo '</select>';
+	}
+	catch(PDOException $e){
+		die('<p> La connexion a échoué. Erreur['.$e->getCode().'] : '.$e->getMessage().'</p>');
+	}
+}
+
+
+function code($texte)
+{
+	//Smileys
+	$texte = str_replace(':D ', '<img src="./images/smileys/heureux.gif" title="heureux" alt="heureux" />', $texte);
+	$texte = str_replace(':lol: ', '<img src="./images/smileys/lol.gif" title="lol" alt="lol" />', $texte);
+	$texte = str_replace(':triste:', '<img src="./images/smileys/triste.gif" title="triste" alt="triste" />', $texte);
+	$texte = str_replace(':frime:', '<img src="./images/smileys/cool.gif" title="cool" alt="cool" />', $texte);
+	$texte = str_replace(':rire:', '<img src="./images/smileys/rire.gif" title="rire" alt="rire" />', $texte);
+	$texte = str_replace(':s', '<img src="./images/smileys/confus.gif" title="confus" alt="confus" />', $texte);
+	$texte = str_replace(':O', '<img src="./images/smileys/choc.gif" title="choc" alt="choc" />', $texte);
+	$texte = str_replace(':question:', '<img src="./images/smileys/question.gif" title="?" alt="?" />', $texte);
+	$texte = str_replace(':exclamation:', '<img src="./images/smileys/exclamation.gif" title="!" alt="!" />', $texte);
+
+	//Mise en forme du texte
+	//gras
+	$texte = preg_replace('`\[g\](.+)\[/g\]`isU', '<strong>$1</strong>', $texte); 
+	//italique
+	$texte = preg_replace('`\[i\](.+)\[/i\]`isU', '<em>$1</em>', $texte);
+	//souligné
+	$texte = preg_replace('`\[s\](.+)\[/s\]`isU', '<u>$1</u>', $texte);
+	//lien
+	$texte = preg_replace('#http://[a-z0-9._/-]+#i', '<a href="$0">$0</a>', $texte);
+	//etc., etc.
+	$texte = preg_replace('`\[quote auteur=([a-z0-9A-Z._-]+) \](.+)\[/quote\]`isU', '<div id="quote">Auteur : $1 </ br> $2 </div>', $texte);
+
+	
+
+	//On retourne la variable texte
+	return $texte;
+}
+
+
+function verif_auth($auth_necessaire)
+{
+	$level=(isset($_SESSION['level']))?$_SESSION['level']:1;
+	return ($auth_necessaire <= intval($level));
+}
+
+
+function erreur($err='')
+{
+   $mess=($err!='')? $err:'Une erreur inconnue s\'est produite';
+   exit('<p>'.$mess.'</p>
+   <p>Cliquez <a href="./Accueil.php">ici</a> pour revenir à la page d\'accueil</p></body></html>');
+}
+?>
+
+
+?>
+
+
+
--- Rendez-vous.php
+++ Rendez-vous.php
@@ -1,14 +1,205 @@
-<?php require('head.php');?>
-	<!-- Nom des onglets -->
-		<title>Rendez-vous</title>
-<?php require('body.php');?>
-<?php
-	session_start();
+<?php session_start(); ?>
+
+
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		  <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title> Rendez-vous </title>
+		<link rel="stylesheet" href="Projet.css"/>
+		<link rel="stylesheet" href="provisoire.css"/>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		<script type="text/javascript">
+			jQuery(function($){
+				$('.month').hide();
+				$('.month:first').show();
+				$('.months a:first').addClass('active');
+				var current =1;
+				$('.months a').click(function(){
+					var month = $(this).attr('id').replace('linkMonth','');
+					if(month != current){
+						$('#month'+current).slideUp();
+						$('#month'+month).slideDown();
+						$('.months a').removeClass('active');
+						$('.months a#linkMonth'+month).addClass('active');
+						current = month;
+						
+					}
+					return false;
+				});
+			});
 			
-	if(!isset($_SESSION['connexion']))
-	{
-		header("Location:Connexion.php");
-		exit();
-	}
+			function radioclick (b) {
+				document.getElementById('div1').style.display = (b=="vous"? 'none':'block');
+				document.getElementById('div2').style.display = (b=="gEtudiant"? 'none':'block');
+
+			}	
+		</script>
+	</head>
+	<body>
+
+<?php
+require('cnx.php');
+require('Fonctions.php');
+
+	$date = new Date();
+	$year = date('Y');
+	$eventsDest = $date->getEventsDest($year, $_SESSION['email']);
+	$eventsExpe = $date->getEventsExpe($year, $_SESSION['email']);
+	echo '<p> '. print_r($eventsDest) .' </p>';
+	echo '<p> '. print_r($eventsExpe) .' </p>';
+	$dates = $date->getAll($year);
+	print_r($_SESSION);
+	
 ?>
-<?php require('footer.php');?>
+	<?php require('Navigation.php'); ?>
+	
+	<div class="row">
+	<div class="col-md-3 formEvent" style="padding-top: 100px; padding-left:40px;">
+	
+	<h3>Ajouter un événement</h3>
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+			<p >
+				
+				Date <br> <input type="text" id="date" name="date" readonly ><br>
+				Heure <input type="time" name="time" style="margin-top: 8px;"></br>
+				
+				<?php /* Les possibilités*/
+					if($_SESSION['categorie'] == 'professeur'){
+				?>
+				<p style="padding-top: 8px; padding-left: 8px;">
+				Evenement pour: <br>
+				vous <input type="radio" name="categ" value="vous" onclick="radioclick(this.value);" /><br>
+				etudiant <input type="radio" name="categ" value="gEtudiant" onclick="radioclick(this.value);" /><br>
+				</p>
+				<p  id="div1" style="display: none; margin-left: 30px;">
+					<?php genereListeEtudiant(); ?>
+				</p>
+				
+				<?php } ?>
+				nom de l'événement <br> <input type="text" name="event"><br>
+				<input type="submit" value="soumettre">
+				
+				
+			</p>
+		</form>
+	</div>
+	
+		<div class="col-md-9">
+			<!--		a rajouter: le fait de pouvoir changer de mois en appuyant sur une fleche -->
+			<div class="year">
+				<?php echo $year; ?>
+			</div>
+			
+					<div class="months">
+						<ul >
+						<?php 
+							foreach($date->months as $id =>$m){?>
+							<li ><a href="#" id="linkMonth<?php echo $id+1; ?>"><?php echo utf8_encode(substr(utf8_decode($m),0,3));?></a></li>
+						<?php } ?>
+						</ul>
+					</div>
+			<div class="clear"></div>
+				<?php foreach($dates[$year] as $m => $days){ ?>
+					<div class="month" id="month<?php echo $m; ?>">
+						<table>
+						<!-- affiche les 3 premieres lettres de chaque jour dans l'entete --> 
+							<tr>
+								<?php foreach($date->days as $d){ ?>
+									<th class="nomJour"><?php echo substr($d,0,3); ?></th>
+								<?php } ?>
+							</tr>
+						<!-- permet de mettre le premier jour au bonne endroit sur le calendrier -->
+							<tr>
+								<?php $end = end($days); foreach($days as $d => $w){ ?>
+									<?php $time = strtotime("$year-$m-$d"); ?>
+									<?php if($d ==1){ ?>
+										<td class="padding" colspan="<?php echo $w-1; ?>"></td>
+									<?php }?>
+									
+						<!-- affiche le jour et permet ensuite de sauter une ligne quand on arrive a dimanche -->
+									<td >
+										<div class="relative">
+										<div class="day">
+										<a onclick="document.getElementById('date').value='<?php echo $year ."-" .$m. "-".$d; ?>';" >
+										<?php echo $d; ?>
+										</div>
+										</div>
+										<div class="daytitle">
+											<?php echo $date->days[$w-1]; ?> <?php echo $d; ?> <?php echo $date->months[$m-1]; ?>
+										</div>
+											<ul class="events" style="color: brown;" >
+												<?php if(isset($eventsDest[$time])){
+													foreach($eventsDest[$time] as $e){ ?>
+														<li class="destination"><?php echo $e; ?></li>
+												<?php }
+												} ?>
+											</ul>
+											<ul class="events" style="color: green;">
+												<?php if(isset($eventsExpe[$time])){
+													foreach($eventsExpe[$time] as $e){ ?>
+														<li class="expedition"><?php echo $e; ?></li>
+												<?php }
+												} ?>
+											</ul>
+										</a>
+										
+									</td>
+									
+								<?php if($w == 7) echo '</tr><tr>'; } ?>
+								
+								<?php 
+								if($end != 7){ ?>
+									<td class="padding" colspan="<?php echo 7-$end; ?>"></td>
+								<?php } ?>
+							</tr>
+						</table>
+					</div>
+				<?php } ?>
+		
+			
+		</div>
+	
+	</div>
+	
+<?php
+print_r($_POST);
+	
+	if(isset($_POST['date']) && isset($_POST['event']) && isset($_POST['categ']) && isset($_POST['selection']) && $_POST['time']){
+		if(trim($_POST['date']) != '' && trim($_POST['event']) != ''){
+			$title = htmlspecialchars($_POST['event'], ENT_NOQUOTES);
+			$mailUtilisateur = htmlspecialchars($_SESSION['email'], ENT_NOQUOTES);
+			
+			if($_SESSION['categorie'] =="etudiant" || $_POST['categ'] =="vous"){
+				ajouterEventPerso($_POST['date'], $title, $mailUtilisateur);
+			}
+			else if(trim($_POST['categ']) != '' && trim($_POST['selection']) != ''){
+				ajouterEventProfesseur($_POST['date'], $title, $mailUtilisateur, $_POST['selection']);
+			}
+			
+		}
+	}
+	
+	
+
+
+	
+	
+/*	echo '<p> '. print_r($events) .'</p>';
+	echo '<p> '. print_r($dates) .'</p>';
+	*/
+	/*
+		39:47
+	*/
+
+
+?>
+
+</body>
+</html>
+
+<?php require('Footer.php'); ?>
