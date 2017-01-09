62eddbf6da3c1ae2e96d2c25d5142b948dd9bd28
Glacia93160 <truong.viviane@gmail.com>
2017-01-10 00:20:37 +0100

    ../

--- ressouce/body.php
+++ ressouce/body.php
@@ -1,13 +1,13 @@
 <!-- Bootstrap -->
 	
-		<link href="../../../css/provisoire.css" rel="stylesheet">
-		<link href="../../../css/bootstrap.css" rel="stylesheet" >
-		<link rel="shortcut icon" href="../../../image/Logo_IUT_Villetaneuse.png"/>
+		<link href="../css/provisoire.css" rel="stylesheet">
+		<link href="../css/bootstrap.css" rel="stylesheet" >
+		<link rel="shortcut icon" href="../image/Logo_IUT_Villetaneuse.png"/>
 
-		<script src="../../../js/bootstrap-dropdown.js"></script>
+		<script src="../js/bootstrap-dropdown.js"></script>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-		<script src="../../../js/bootstrap.min.js"></script>
-		<script src="../../../js/bootstrap.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/bootstrap.js"></script>
 	</head>
 		
 	<body>
@@ -17,7 +17,7 @@
 			<div class="container-fluid"> 
 				
 				<ul class="nav navbar-nav">
-					<li><a href="../../../Accueil.php"><img id="logo" src="../../../image/Logo_IUT_Villetaneuse.png" alt="Accueil"></a></li>
+					<li><a href="../Accueil.php"><img id="logo" src="../image/Logo_IUT_Villetaneuse.png" alt="Accueil"></a></li>
 					<li class="dropdown navigation">
 						<a href="../../Formation.php" class="dropdown-toogle">Formation</a>
 						<ul class="dropdown-menu" role="menu">
@@ -29,12 +29,12 @@
 							<li><a href="../GEII.php">Formations en Électronique, Électricité, Informatique Industrielle, Nanotechnologies</a></li>
 						</ul>
 					</li>			
-					<li class="navigation"><a href="../../../Ressource.php">Ressource</a></li>
-					<li class="navigation"><a href="../../../Travail.php">Espace de travail</a></li>
-					<li class="navigation"><a href="../../../Rendez-vous.php">Rendez-vous</a></li>
-					<li class="navigation"><a href="../../../Actualite.php">Actualites</a></li>
-					<li class="navigation"><a href="../../../Forum.php">Forum</a></li>   
-					<li class="navigation"><a href="../../../Connexion.php"><span class="glyphicon glyphicon-user"></span> Connexion</a></li>
+					<li class="navigation"><a href="Ressource.php">Ressource</a></li>
+					<li class="navigation"><a href="../Travail.php">Espace de travail</a></li>
+					<li class="navigation"><a href="../Rendez-vous.php">Rendez-vous</a></li>
+					<li class="navigation"><a href="../Actualite.php">Actualites</a></li>
+					<li class="navigation"><a href="../Forum.php">Forum</a></li>   
+					<li class="navigation"><a href="../Connexion.php"><span class="glyphicon glyphicon-user"></span> Connexion</a></li>
 				</ul>
 					
 				<div id="recherche">
