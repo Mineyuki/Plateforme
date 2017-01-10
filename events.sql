32c949c4af9ea7fbc508c27d527e5e127a9cfbd4
Thelegendarry <co.steph.ballet@gmail.com>
2017-01-10 01:31:09 +0100

    table pour les evenements et le forum actualiser

---
+++ events.sql
@@ -0,0 +1,9 @@
+CREATE TABLE 'events' (
+ 'id' bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+ 'mailExpe' varchar(50) DEFAULT NULL,
+ 'mailDest' varchar(50) DEFAULT NULL,
+ 'DateH' datetime DEFAULT NULL,
+ 'title' varchar(255) DEFAULT NULL,
+ PRIMARY KEY ('id'),
+ UNIQUE KEY 'id' ('id')
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
