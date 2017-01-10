908bc40005bd9ed29ec4b11988250f252fc4ea25
Glacia93160 <truong.viviane@gmail.com>
2017-01-10 03:14:28 +0100

    resout pb sup.php

--- ressouce/rEEIIN.php
+++ ressouce/rEEIIN.php
@@ -21,12 +21,14 @@
 <?php
 
 //afficher les fichiers
+$nom='rEEIIN.php';
 $dir = '/upload/rEEIIN/';
 if(is_dir($dir)) {
         if ($dh = opendir($dir)) {
         while (($file = readdir($dh)) !== false) {
                 if($file!="." && $file!="..") {
-                echo "<li><a href='".$dir.$file."'>".$file."</a><a href='supprimer.php?name={$file}&url={$dir}'><span class=\"glyphicon glyphicon-remove\"></span></a></li>";
+                echo "<li><a href='".$dir.$file."'>".$file."</a><a href='./supprimer.php?name={$file}&url={$dir}&formation={$nom}'><span class=\"glyphicon glyphicon-remove\"></span></a></li>";
+
             }
         }
         closedir($dh);
