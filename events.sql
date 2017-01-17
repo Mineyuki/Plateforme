eaa166ed6876daef60a180dc8740780ba8fd8ff8
Thelegendarry <co.steph.ballet@gmail.com>
2017-01-17 02:57:19 +0100

    ajout d'une colonne sent permettant d'envoyer des notifs

--- events.sql
+++ events.sql
@@ -1,9 +1,10 @@
-CREATE TABLE events (
- id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
- mailExpe varchar(50) DEFAULT NULL,
- mailDest varchar(50) DEFAULT NULL,
- DateH datetime DEFAULT NULL,
- title varchar(255) DEFAULT NULL,
- PRIMARY KEY (id),
- UNIQUE KEY id (id)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+CREATE TABLE 'events' (
+ 'id' bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+ 'mailExpe' varchar(50) DEFAULT NULL,
+ 'mailDest' varchar(50) DEFAULT NULL,
+ 'sent' tinyint(1) DEFAULT '0',
+ 'DateH' date DEFAULT NULL,
+ 'heure' time DEFAULT NULL,
+ 'title' varchar(255) DEFAULT NULL,
+ PRIMARY KEY ('id'),
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1
