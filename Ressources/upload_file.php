a132311727f43a9abfc6e9b447947fa4bf7a1917
Glacia93160 <truong.viviane@gmail.com>
2017-01-17 09:23:13 +0100

    ajout + commentaires +etc

--- Ressources/upload_file.php
+++ Ressources/upload_file.php
@@ -25,8 +25,13 @@ try{
           
           //enregistre les fichiers avec leur dossier correspendant
          if($_SESSION["formation"]=="MDI"){
-          //verifier que l'existence du fichier
-             if (file_exists("upload/rMDI/" . $_FILES["file"]["name"]))
+          //verifier que le dossier existe, sinon on cree un
+		if (!file_exists('upload/rMDI/')
+                {
+                mkdir("upload/rMDI/");   
+   }
+               //verifier que l'existence du fichier
+             		if (file_exists("upload/rMDI/" . $_FILES["file"]["name"]))
                 {
                  echo "<script> {window.alert('Un fichier du même nom existe déjà');location.href='Ressource.php'} </script>";    
   
@@ -42,56 +47,81 @@ try{
             }
   
           elseif($_SESSION["formation"]=="GCRHM"){
-  
+ //verifier que le dossier existe, sinon on cree un
+		if (!file_exists('upload/rGCRHM/')
+                {
+                mkdir("upload/rGCRHM/");   
+  }
+  //verifier que l'existence du fichier
 		          if (file_exists("upload/rGCRHM/" . $_FILES["file"]["name"])){
   			echo "<script> {window.alert('Un fichier du même nom existe déjà ');location.href='Ressource.php'} </script>";
  		 }
 
   		else{
+//on enregistre
 			 move_uploaded_file($_FILES["file"]["tmp_name"], "upload/rGCRHM/" . $_FILES["file"]["name"]);
  			 echo "Stored in: " . "upload/rGCRHM/" . $_FILES["file"]["name"];
  
   		}
  	}
 
+
  	 elseif($_SESSION["formation"]=="JHF"){
- 		
+ //verifier que le dossier existe, sinon on cree un
+		if (!file_exists('upload/rJHF/')
+                {
+                mkdir("upload/rJHF/");   
+ }
+//verifier que l'existence du fichier
 		if (file_exists("upload/rJHF/" . $_FILES["file"]["name"])){
  
 			 echo "<script> {window.alert('Un fichier du même nom existe déjà ');location.href='Ressource.php'} </script>";
   		}
 
   		else{
+//on enregistre
 			move_uploaded_file($_FILES["file"]["tmp_name"],"upload/rJHF/" . $_FILES["file"]["name"]);
   			echo "Stored in: " . "upload/rJHF/" . $_FILES["file"]["name"];
  
   		}
-  	}
-  
+  			
+  }
+
  	elseif($_SESSION["formation"]=="ISL"){
-  		
+ //verifier que le dossier existe, sinon on cree un
+	if (!file_exists('upload/rISL/')
+                {
+                mkdir("upload/rISL/");   
+  		 }
+//verifier que l'existence du fichier
 		if (file_exists("upload/rISL/" . $_FILES["file"]["name"])){
  		
 			 echo "<script> {window.alert('Un fichier du même nom existe déjà ');location.href='Ressource.php'} </script>";
  		 }
   
 		else{
-
+//on enregistre
   			move_uploaded_file($_FILES["file"]["tmp_name"],"upload/rISL/" . $_FILES["file"]["name"]);
  			echo "Stored in: " . "upload/rISL/" . $_FILES["file"]["name"];
  
   		}
  	 }
+
  	 
   	elseif($_SESSION["formation"]=="RT"){
-  
+ //verifier que le dossier existe, sinon on cree un
+		if (!file_exists('upload/rRT/')
+                {
+                mkdir("upload/rRT/");   
+		 }
+  //verifier que l'existence du fichier
 		if (file_exists("upload/rRT/" . $_FILES["file"]["name"])){
   
 			echo "<script> {window.alert('Un fichier du même nom existe déjà ');location.href='Ressource.php'} </script>";
   		}
  
 		 else{
-  
+  //on enregistre
 			move_uploaded_file($_FILES["file"]["tmp_name"],"upload/rRT/" . $_FILES["file"]["name"]);
   			echo "Stored in: " . "upload/rRT/" . $_FILES["file"]["name"];
  
@@ -99,7 +129,12 @@ try{
  	 }
   
  	 elseif($_SESSION["formation"]=="EEIIN"){
-  
+ //verifier que le dossier existe, sinon on cree un
+		if (!file_exists('upload/rEEIIN/')
+                {
+                mkdir("upload/rEEIIN/");   
+		 }
+  //verifier que l'existence du fichier
 		if (file_exists("upload/rEEIIN/" . $_FILES["file"]["name"])){
 
   		echo "<script> {window.alert('Un fichier du même nom existe déjà ');location.href='Ressource.php'} </script>";
@@ -107,7 +142,7 @@ try{
 		}
  	
 		else{
-  
+  //on enregistre
 			move_uploaded_file($_FILES["file"]["tmp_name"],"upload/rEEIIN/" . $_FILES["file"]["name"]);
  			echo "Stored in: " . "upload/rEEIIN/" . $_FILES["file"]["name"];
  
